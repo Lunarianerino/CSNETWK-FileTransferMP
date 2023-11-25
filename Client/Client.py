45b85e12c4f68bf4c2ff087bed4c2006327cbc28
Lunarianerino <71534058+Lunarianerino@users.noreply.github.com>
2023-11-26 00:14:46 +0800

    bonus nalang kulang :3

--- Client/Client.py
+++ Client/Client.py
@@ -2,56 +2,218 @@
 from socket import *
 import sys # In order to terminate the program
 import threading
-class Client(threading.Thread):
-    def __init__(self, HOST, PORT):
-        threading.Thread.__init__(self)
+import os
+import argparse
+import time
+class Client():
+    def __init__(self, HOST = None, PORT = None):
+        self.HOST = HOST
+        self.PORT = PORT
+        self.connection_flag = False
+        #self.start()
+
+    def connect(self, HOST, PORT):
         self.HOST = HOST
         self.PORT = PORT
         self.clientEndpoint = socket()
-        self.clientEndpoint.connect((self.HOST, int(self.PORT)))
+        try:
+            self.clientEndpoint.connect((self.HOST, int(self.PORT)))
+        except Exception as e:
+            self.HOST = None
+            self.PORT = None
+            return False
         self.buffer_size = 1024
+        self.timeout = 5
         self.connection_flag = True
-        self.start()
+        return True
 
     def run(self):
+        while self.connection_flag == False:
+            continue #Do nothing until connection is established
+
         while self.connection_flag:
+            print("Waiting for reply...")
             try:
                 reply = self.clientEndpoint.recv(self.buffer_size).decode()
-                if not self.HandleStatus(reply):
-                    print(reply)
+                self.HandleStatus(reply)
             except Exception as e:
                 print(e)
                 break  
 
-    def sendMessage(self, message):
-        self.clientEndpoint.send(str(message).encode())
+    def sendMessage(self, *args):
+        message = " ".join(args)
+        print(message)
+        self.clientEndpoint.send(message.encode())
     def close(self):
         self.clientEndpoint.close()
         self.buffer_size = 0
         self.connection_flag = False
         
     def HandleStatus(self, status):
+        status, message = status.split("|", 1)
         if status == "444 CONNECTION_CLOSED":
-            print("Connection closed. Thank you!")
-            self.close()
-            return True
-        return False
-if __name__ == "__main__":
-    client = None
-    while True:
-        text = input(">> ")
-        try:
-            if text[0] == '/':
-                command = text[1:].split()
-                if command[0]== "join":
-                    client = Client(command[1], command[2])
-                elif client != None:
-                    client.sendMessage(text)
+            self.connection_flag = False
+            self.HOST = None
+            self.PORT = None
+        #print(status)
+        print(message)
+        return status, message
+    def commandHandler(self, command, *args):
+        if command == "/join":
+
+            #check if args is greater than 2
+            if len(args[0]) != 2:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            
+            print("Joining...")
+            #print(args)
+            if self.connect(args[0][0], args[0][1]):
+                status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
             else:
-                if client != None:
-                    client.sendMessage(text)
-        except Exception as e:
+                print("Error: Connection to the Server has failed! Please check IP Address and Port Number.")
+        elif command == "/register":
+            if len(args[0]) < 1:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Error: Registration failed. Please connect to the server first.")
+                return
+            handle = " ".join(args[0])
+            self.sendMessage(command, handle)
+            status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+        elif command == "/leave":
+            if len(args[0]) > 0:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Error: Disconnection failed. Please connect to the server first.")
+            else:
+                self.sendMessage(command)
+                reply = self.clientEndpoint.recv(self.buffer_size).decode()
+                self.HandleStatus(reply)
+        elif command == "/?":
+            if len(args[0]) > 0:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Client Commands: \n/join <IP Address> <Port Number> - Connect to the server application")
+                print("/? - Request command help to output all Input Syntax commands for references")
+                print("More commands will be available once connected to the server application")
+            else:
+                print("File Transfer Commands:")
+                print("/leave - Disconnect to the server application")
+                print('/register <handle> - Register a handle to the server')
+                print("/store <filename> - Send file to server")
+                print("/dir - Request directory file list from a server")
+                print("/get <filename> - Request file from server")
+                print("/? - Request command help to output all Input Syntax commands for references")
+                
+        elif command == "/store":
+            if len(args[0]) < 1:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Error: Command failed. Please connect to the server first.")
+                return
+            filename = " ".join(args[0])
+            print(f"Sending file {filename}...")
+            if os.path.exists(filename):
+                print("File exists.")
+                self.sendMessage(command, filename)
+                status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+                if status == "200 OK":
+                    self.sendMessage(str(os.path.getsize(filename)))
+                    status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+                    f = open(filename, 'rb')
+                    data = f.read()
+                    self.clientEndpoint.send(data)
+                    f.close()
+                    status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+            else:
+                print("Error: File not found.")
+        elif command == "/get":
+            if len(args[0]) < 1:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Error: Command failed. Please connect to the server first.")
+                return
+            filename = " ".join(args[0])
+            print(f"Getting file {filename}...")
+            self.sendMessage(command, filename)
+            status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+            if status == "200 OK":
+                filesize = int(message)
+                self.sendMessage("200 OK|File size received")
+                i = 1
+                if os.path.exists(f"./Downloads/{filename}"): #aaron pic.jpg
+                    ext = filename.split(".")[-1] #.jpg
+                    filename = ".".join(filename.split(".")[:-1]) #aaron pic
+                    while os.path.exists(f"./Downloads/{filename}({i}).{ext}"): #aaron pic(1).jpg
+                        i+=1
+                    filename = f"{filename}({i})" + "." + str(ext)
+
+
+                open(f"./Downloads/{filename}", 'x')
+                f = open(f"./Downloads/{filename}", 'wb')
+
+                totalRecv = 0
+
+                try:
+                    while totalRecv < filesize:
+                        data = self.clientEndpoint.recv(self.buffer_size)
+                        totalRecv += len(data)
+                        f.write(data)
+                        print(f"Progress: {totalRecv/filesize*100}%")
+                    f.close()
+                    self.clientEndpoint.send("200 OK|File transfer complete".encode())
+                except Exception as e:
+                    print(e)
+                    self.clientEndpoint.send("409 CONFLICT|File transfer failed".encode())
+                    print("Error: File transfer failed.")
+                    return
+                
+                status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+        elif command == "/dir":
+            if len(args[0]) > 0:
+                print("Error: Command parameters do not match or is not allowed.")
+                return
+            if self.connection_flag is False:
+                print("Error: Command failed. Please connect to the server first.")
+                return
+            self.sendMessage(command)
+            status, message = self.HandleStatus(self.clientEndpoint.recv(self.buffer_size).decode())
+
+        else:
+            self.sendMessage(command, " ".join(args[0]))
+            reply = self.clientEndpoint.recv(self.buffer_size).decode()
+            self.HandleStatus(reply)
+            # self.sendMessage(args[0][0])
+            # self.sendFile(args[0][0])
+            
+if __name__ == "__main__":
+    client = Client()
+    while True:
+        try:
+            text = input(">> ")
+            if text[0] == '/':
+                command = text.split()
+                client.commandHandler(command[0], command[1:])
+            else:
+                #if client != None:
+                #    client.sendMessage(text)
+                print("Error: Command not found. Please use /? to view all commands.")
+        except KeyboardInterrupt:
+            print("Closing connection...")
+            break
             #TODO: might have to implement a list of errors for easy error messages in GUI
-            print("Connection to the Server has failed! Please check IP Address and Port Number.") 
+        except Exception as e:
+            print (e)
+    print("Process interrupted")
+    try:
+        exit(0)
+    except SystemExit:
+        os._exit(0)
 
         
--- Server/Server.py
+++ Server/Server.py
@@ -4,6 +4,7 @@ import sys # In order to terminate the program
 import threading
 import time
 import os
+import json
 class SocketThread(threading.Thread):
     def __init__(self, connection):
         threading.Thread.__init__(self)
@@ -12,6 +13,8 @@ class SocketThread(threading.Thread):
         self.server = connection["Server"]
         self.start()
         self.Names = list()
+        self.timeout = 5 #seconds
+        self.connectionSocket.send("200 OK|Connection to the File Exchange Server is successful!".encode())
     def run(self):
         i = 0
         while True:
@@ -33,55 +36,216 @@ class SocketThread(threading.Thread):
             #i += 1
     def send(self, message):
         self.connectionSocket.send(message.encode())
+
+
+    #function to check if a socket is in the list of names
+    def checkHandle(self, port):
+        for name in self.server.getNames():
+            if str(name["Port"]) == str(port):
+                return name["Name"]
+        return None
+    
+    def checkIfRegistered(self, port):
+        for name in self.server.getNames():
+            if str(name["Port"]) == str(port):
+                return True
+        return False
+    
+    def statusHandler(self, status):
+        status, message = status.split("|", 1)
+        print(status)
+        print(message)
+        return status, message
+    
     def commandHandler(self, command, *args):
         print(command)
         if command == "leave":
-            self.connectionSocket.send("444 CONNECTION_CLOSED".encode())
-            self.connectionSocket.close()
-            self.server.ActiveConnections.remove(self)
+            print("Closing connection...")
+            self.connectionSocket.send("444 CONNECTION_CLOSED|Connection closed. Thank you!".encode())
+            sys.exit() #close thread
+            #self.server.ActiveConnections.remove(self)
             
         elif command == "broadcast":
+            #check first if user is registered
+            if(self.checkIfRegistered(self.connectionSocket.getpeername()[1]) == False):
+                self.connectionSocket.send("401 UNAUTHORIZED|You must first use /register to access this command. Enter /? for more details.".encode())
+                return
+
+            
             self.server.broadcast(args[0])
             self.connectionSocket.send("200 OK".encode())
 
         elif command == "register":
+            #check if args[0] exists
+            str_name = " ".join(args[0])
+            if len(str_name) == 0:
+                self.connectionSocket.send("400 BAD REQUEST|No handle provided.".encode())
+                return
+
+            #check if str_name is already in use
+            for name in self.server.getNames():
+                if name["Name"] == str_name:
+                    self.connectionSocket.send("409 CONFLICT|Error: Registration failed. Handle or alias already exists.".encode())
+                    return
+            
             name = {
-                "Name": args[0],
-                "Socket": self.connectionSocket
+                "Name": str_name,
+                "Port": self.connectionSocket.getpeername()[1]
             }
+
             self.server.addName(name)
-            self.connectionSocket.send("200 OK".encode())
-            self.connectionSocket.send(f"Welcome {args[0]}".encode())
+            self.connectionSocket.send(f"200 OK|Welcome, {str_name}!".encode())
+            print("Server Names: ")
             print(self.server.getNames())
 
-        elif command == "?":
-            self.connectionSocket.send("200 OK".encode())
-            self.connectionSocket.send("Commands:\n/join <server_ip_add> <port>\n/leave\n/register <handle>\n/store <filename>\n/dir\n/get <filename>\n/?".encode())
-
         elif command == "dir":
+            #check first if user is registered
+            if(self.checkIfRegistered(self.connectionSocket.getpeername()[1]) == False):
+                self.connectionSocket.send("401 UNAUTHORIZED|You must first use /register to access this command. Enter /? for more details.".encode())
+                return
             #get the list of files in /Server/Files
-            directory = './Server/Files'
+            if len(args[0]) > 0:
+                self.connectionSocket.send("400 BAD_REQUEST|Error: Command parameters do not match or is not allowed.".encode())
+                return
+            directory = r'./Server/Files'
             files = os.listdir(directory)
             file_list = [] #list of dicts for each file
             for f in files:
-                #tokenize file name with this format [Date][Time][Uploader][File Name including extension]
-                #example: [2023-11-17][16-48-05][John][Hello World]
-                tokens = f.replace('[', '').replace(']', '\n').split('\n')
-                file = { #I wanted to use this dict for GUI but apparently CLI only needs to display file name and I may have overdone things
-                    "Date": tokens[0],
-                    "Time": tokens[1].replace('-', ':'),
-                    "Uploader": tokens[2],
-                    "Name": ''.join(tokens[3:]),
-                    "Size": str(os.path.getsize(directory + '/' + f)) + " Bytes"
-                }
-                file_list.append(file)
+                file_list.append({"Name": f})
             print(file_list)
-            self.connectionSocket.send("200 OK".encode())
-            self.connectionSocket.send("Server Directory\n".encode())
+            string_to_send=""
             for filename in file_list: # TODO: change this to sending the entire object instead of just the name once GUI is implemented
-                self.connectionSocket.send(f"{filename['Name']}\n".encode())
+                string_to_send = string_to_send + f"{filename['Name']}\n"
+            
+            self.connectionSocket.send(("200 OK|Server Directory: \n" + string_to_send).encode())
+        
+        elif command == "get":
+            #check first if user is registered
+            if(self.checkIfRegistered(self.connectionSocket.getpeername()[1]) == False):
+                self.connectionSocket.send("401 UNAUTHORIZED|You must first use /register to access this command. Enter /? for more details.".encode())
+                return
+            filename = " ".join(args[0])
+            
+            directory = r'./Server/Files/'
+            files = os.listdir(directory)
+
+            #if file exists, send file
+            if filename in files:
+                self.connectionSocket.send(f"200 OK|{str(os.path.getsize(directory+filename))}".encode())
+                status, message = self.statusHandler(self.connectionSocket.recv(1024).decode())
+                if status != "200 OK":
+                    return
+                f = open(f"./Server/Files/{filename}", 'rb')
+                data = f.read()
+                self.connectionSocket.send(data)
+                f.close()
+                status, message = self.statusHandler(self.connectionSocket.recv(1024).decode())
+                if status == "200 OK":
+                    self.connectionSocket.send(f"200 OK|File received from Server: {filename}".encode())
+                else:
+                    self.connectionSocket.send("409 CONFLICT|Something went wrong while transferring data...".encode())
+            else:
+                self.connectionSocket.send("404 NOT FOUND|Error: File not found in server".encode())
+
+        elif command == "store":
+
+            #TODO: insert check here if user has registered
+            print(f"Checking user: {self.connectionSocket.getpeername()[1]}")
+            user = self.checkHandle(self.connectionSocket.getpeername()[1])
+            print(f"user: {user}")
+            if user == None:
+                self.connectionSocket.send("401 UNAUTHORIZED|You must first use /register to access this command. Enter /? for more details.".encode())
+                return
+            self.connectionSocket.send(f"200 OK|User {user} found.".encode())
+        
+            filename = " ".join(args[0])
+            filename = filename.split("/")[-1]
+            print(f"Receiving file {filename}...")
+
+            curr_timeout = 0
+            received = False
+            try:
+                while curr_timeout < self.timeout:
+                    filesize = int(self.connectionSocket.recv(1024).decode())
+                    if filesize > 0:
+                        received = True
+                        break
+                    time.sleep(1)
+            except Exception as e:
+                print(e)
+                self.connectionSocket.send("409 CONFLICT|Something went wrong while transferring data...".encode())
+                return
+            
+            if received:
+                curr_timeout = 0
+                self.connectionSocket.send("200 OK|File Size Received".encode())
+            else:
+                self.connectionSocket.send("409 CONFLICT|File Size not received, aborting...".encode())
+                return
+            
+            print(f"Filesize: {filesize}")
+
+
+
+            # #self.connectionSocket.send("File has been uploaded".encode())
+            # #checking for duplicate files
+            i = 1
+            if os.path.exists(f"./Server/Files/{filename}"): #aaron pic.jpg
+                ext = filename.split(".")[-1] #.jpg
+                filename = ".".join(filename.split(".")[:-1]) #aaron pic
+                while os.path.exists(f"./Server/Files/{filename}({i}).{ext}"): #aaron pic(1).jpg
+                    i+=1
+                filename = f"{filename}({i})" + "." + str(ext)
+
+            file = {
+                "Name": filename,
+                "Uploader": user,
+                "DateTime" : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                "Size": filesize
+            }
+
+            try:
+                open(f"./Server/Files/{filename}", 'x')
+                f = open(f"./Server/Files/{filename}", 'wb')
+
+                totalRecv = 0
+
+                while totalRecv < filesize:
+                    curr_timeout = 0
+                    while curr_timeout < self.timeout:
+                        data = self.connectionSocket.recv(1024)
+                        curr_timeout += 1
+                        if len(data) > 0:
+                            break
+                        
+                        time.sleep(1)
+                    totalRecv += len(data)
+                    f.write(data)
                 
+                print("Download Complete!")
+                f.close()
+            except Exception as e:
+                print(e)
+                self.connectionSocket.send("409 CONFLICT|Something went wrong while receiving data...".encode())
+                return
+            
+            self.connectionSocket.send(f"200 OK|{file['Uploader']} <{file['DateTime']}>: Uploaded {file['Name']}".encode())
+            #FIXME: CHANGE TO BROADCAST (SHOULD BE SEEN BY ALL CLIENTS)
+
+            #append file to FileList.json (Will be used for GUI)
+            try:
+                with open('./Server/Storage/FileList.json', 'r') as f:
+                    data = json.load(f)
+            except json.decoder.JSONDecodeError:
+                data = []
+            data.append(file)
+            with open('./Server/Storage/FileList.json', 'w') as f:
+                json.dump(data, f)
         #elif command == "msg":
+
+        else:
+            self.connectionSocket.send("400 BAD REQUEST|Error: Command not found.".encode())
+            return
 
 
 
@@ -127,5 +291,15 @@ class Server:
     def getNames(self):
         return self.Names
 if __name__ == "__main__":
-    server = Server(sys.argv[1], int(sys.argv[2]))
-    server.run()
+    try:
+        server = Server(sys.argv[1], int(sys.argv[2]))
+        server.run()
+    except KeyboardInterrupt:
+        print("Server is closing...")
+        server.ServerSocket.close()
+        sys.exit()
+
+
+
+
+
